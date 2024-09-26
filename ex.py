bfbb74907f383ffc34507ed86290644e1d59017d
Princejohnsonandjohnson <PRaj7@its.jnj.com>
2024-09-26 23:20:16 +0530

    Please add this new change commit

--- ex.py
+++ ex.py
@@ -1,26 +1,23 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
-driver.get("https://jira.jnj.com/browse/AADF-27928")
+driver.get("https://demoqa.com/frames")
 driver.maximize_window()
+driver.implicitly_wait(10)
+frameElement=driver.find_element(By.XPATH,"//iframe[@id='frame2']")
+driver.switch_to.frame(frameElement)
 
-title_locator = (By.XPATH, "//123h1[@id='summary-val']")
-type_locator = (By.XPATH, "//123label[contains(text(),'Type:')]")
-epic_locator = (By.XPATH, "//strong[@title='Epic Link']")
+driver.implicitly_wait(10)
+value=driver.find_element(By.XPATH,"").text
+print('text is',value)
 
-def visible(by_locator):
-    try:
-        elements = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(by_locator))
-        print("this is elements",len(elements))
-        if elements:
-            print("Element is visible on the webpage")
-            return True
-    except:
-        print("Element is not visible")
-        return False
 
-print(visible(epic_locator))
-print(visible(type_locator))
+driver.implicitly_wait(10)
+
+driver.get_screenshot_as_file("prince.png")
+driver.close()
